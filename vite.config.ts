db8402f76e2b45860b2f12ef5b8a6a6a4bfb987b
Sebastian Springer <sebspringer@gmail.com>
2025-09-16 19:31:01 +0200

    test base URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: process.env.NODE_ENV === 'production' ? '/sebspringer/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
